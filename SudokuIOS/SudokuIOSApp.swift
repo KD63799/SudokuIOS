d817a3c0b2fde8524c68036dcad91ab042758487
KD63799 <aninach.beg@gmail.com>
2025-10-20 19:34:05 +0200

    Initial Commit

---
+++ SudokuIOS/SudokuIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuIOSApp.swift
+//  SudokuIOS
+//
+//  Created by Amir on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SudokuIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
